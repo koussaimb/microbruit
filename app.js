d9ca61e6675a9c852c07a4310fb1d7eeaf92d52b
koussaimb <mbirikikoussai0@gmail.com>
2023-04-09 23:37:28 +0200

    add gitignore

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let audioContext;
+const createRNNWasmModule = require('./rnnoise.js').default;
+
+let audioContext = new AudioContext();
 let lowpassFilter;
-
-
 
 const startButton = document.getElementById('start');
 const recordButton = document.getElementById('record');
@@ -12,136 +12,95 @@ const recordedAudio = document.getElementById('recordedAudio');
 const downloadButton = document.getElementById('download');
 const downloadLink = document.getElementById('downloadLink');
 
-
 let localConnection;
 let remoteConnection;
 let localStream;
 let mediaRecorder;
 let chunks = [];
 
-startButton.addEventListener('click', async () => {
-    startButton.disabled = true;
+async function handleStartButtonClick() {
+  startButton.disabled = true;
 
-    // Chargez le modèle RNNoise
-    const rnnoise = new RnNoise();
+  await audioContext.audioWorklet.addModule('processor.js');
+  const rnnoiseProcessor = new AudioWorkletNode(audioContext, 'rnnoise-processor');
 
-    // Créez un scriptProcessor pour traiter l'audio avec RNNoise
-    const bufferSize = 4096;
-    const numberOfInputChannels = 1;
-    const numberOfOutputChannels = 1;
+  const remoteStream = remoteAudio.srcObject;
+  const remoteSource = audioContext.createMediaStreamSource(remoteStream);
+  const remoteDestination = audioContext.createMediaStreamDestination();
+  remoteSource.connect(rnnoiseProcessor);
+  rnnoiseProcessor.connect(remoteDestination);
 
-    const scriptProcessor = audioContext.createScriptProcessor(bufferSize, numberOfInputChannels, numberOfOutputChannels);
-    scriptProcessor.onaudioprocess = (event) => {
-        const inputBuffer = event.inputBuffer.getChannelData(0);
-        const outputBuffer = event.outputBuffer.getChannelData(0);
-        rnnoise.process(inputBuffer, outputBuffer);
-    };
+  const filteredStream = new MediaStream([remoteDestination.stream.getAudioTracks()[0]]);
+  remoteAudio.srcObject = filteredStream;
 
-    // Le reste du code du gestionnaire d'événements ...
-
-    const remoteStream = remoteAudio.srcObject;
-    const remoteSource = audioContext.createMediaStreamSource(remoteStream);
-    const remoteDestination = audioContext.createMediaStreamDestination();
-    remoteSource.connect(scriptProcessor);
-    scriptProcessor.connect(remoteDestination);
-
-    const filteredStream = new MediaStream([remoteDestination.stream.getAudioTracks()[0]]);
-    remoteAudio.srcObject = filteredStream;
-
-    // Configurez MediaRecorder pour enregistrer le flux filtré
-    mediaRecorder = new MediaRecorder(filteredStream);
-    mediaRecorder.ondataavailable = handleDataAvailable;
-    mediaRecorder.onstop = handleStop;
-});
+  // Configurez MediaRecorder pour enregistrer le flux filtré
+  mediaRecorder = new MediaRecorder(filteredStream);
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+  recordButton.disabled = false;
+}
 
 
+startButton.addEventListener('click', handleStartButtonClick);
 
 async function initMicrophone() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        remoteAudio.srcObject = stream;
-        console.log('Microphone activé:', stream);
-    } catch (err) {
-        console.error('Erreur lors de l\'activation du microphone:', err);
-    }
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    remoteAudio.srcObject = stream;
+    console.log('Microphone activé:', stream);
+  } catch (err) {
+    console.error('Erreur lors de l\'activation du microphone:', err);
+  }
 }
+
 initMicrophone();
+
 function handleDataAvailable(event) {
-    if (event.data.size > 0) {
-        chunks.push(event.data);
-    }
+  if (event.data.size > 0) {
+      chunks.push(event.data);
+  }
 }
 
 function handleStop() {
-    const blob = new Blob(chunks, { type: 'audio/webm; codecs=opus' });
-    chunks = [];
-    const audioURL = URL.createObjectURL(blob);
-    recordedAudio.src = audioURL;
-  
-    // Mise à jour du lien de téléchargement
-    downloadLink.href = audioURL;
-    downloadLink.download = 'enregistrement_audio.webm';
-    downloadButton.disabled = false;
-  
+  const blob = new Blob(chunks, { type: 'audio/webm; codecs=opus' });
+  chunks = [];
+  const audioURL = URL.createObjectURL(blob);
+  recordedAudio.src = audioURL;
+
+  // Mise à jour du lien de téléchargement
+  downloadLink.href = audioURL;
+  downloadLink.download = 'enregistrement_audio.webm';
+  downloadButton.disabled = false;
+
+  // Vérifier si l'enregistrement est terminé avant de permettre la lecture
+  recordedAudio.onloadedmetadata = () => {
+    recordButton.disabled = true;
+    stopRecordButton.disabled = true;
+    recordedAudio.play();
     console.log('Enregistrement terminé et audio prêt à être lu.');
   }
+}
 
-// recordButton.addEventListener('click', () => {
-//     mediaRecorder = new MediaRecorder(remoteAudio.srcObject);
-//     mediaRecorder.start();
-//     console.log('Enregistrement en cours...');
-
-//     recordButton.disabled = true;
-//     stopRecordButton.disabled = false;
-
-//     mediaRecorder.ondataavailable = (e) => {
-//         chunks.push(e.data);
-//     };
-
-
-//     mediaRecorder.onstop = () => {
-//         const blob = new Blob(chunks, { type: 'audio/webm; codecs=opus' });
-//         chunks = [];
-//         const audioURL = URL.createObjectURL(blob);
-//         recordedAudio.src = audioURL;
-
-//         // Mise à jour du lien de téléchargement
-//         downloadLink.href = audioURL;
-//         downloadLink.download = 'enregistrement_audio.webm';
-//         downloadButton.disabled = false;
-
-//         console.log('Enregistrement terminé et audio prêt à être lu.');
-//     };
-
-// });
-
-// stopRecordButton.addEventListener('click', () => {
-//     if (mediaRecorder && mediaRecorder.state !== 'inactive') {
-//         mediaRecorder.stop();
-//         recordButton.disabled = false;
-//         stopRecordButton.disabled = true;
-//     }
-// });
 
 recordButton.addEventListener('click', () => {
-    if (mediaRecorder) {
-      mediaRecorder.start();
-      console.log('Enregistrement en cours...');
-  
-      recordButton.disabled = true;
-      stopRecordButton.disabled = false;
-    } else {
-      console.error('MediaRecorder n\'est pas défini.');
-    }
-  });
-  
-  stopRecordButton.addEventListener('click', () => {
-    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
-      mediaRecorder.stop();
-      recordButton.disabled = false;
-      stopRecordButton.disabled = true;
-    }
-  });
+  if (mediaRecorder) {
+    mediaRecorder.start();
+    console.log('Enregistrement en cours...');
+
+    recordButton.disabled = true;
+    stopRecordButton.disabled = false;
+  } else {
+    console.error('MediaRecorder n\'est pas défini.');
+  }
+});
+
+stopRecordButton.addEventListener('click', () => {
+  if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+    mediaRecorder.stop();
+    recordButton.disabled = false;
+    stopRecordButton.disabled = true;
+  }
+});
   
 downloadButton.addEventListener('click', () => {
     downloadLink.click();
---
+++ processor.js
@@ -0,0 +1,27 @@
+registerProcessor('rnnoise-processor', class extends AudioWorkletProcessor {
+    constructor() {
+      super();
+      this._rnnoiseModule = null;
+      this.port.onmessage = async (event) => {
+        if (event.data.type === 'init') {
+          this._rnnoiseModule = await createRNNWasmModule();
+        }
+      };
+    }
+  
+    process(inputs, outputs) {
+      const input = inputs[0];
+      const output = outputs[0];
+      const inputChannel = input[0];
+      const outputChannel = output[0];
+  
+      if (this._rnnoiseModule) {
+        this._rnnoiseModule.process(inputChannel, outputChannel);
+      } else {
+        outputChannel.set(inputChannel);
+      }
+  
+      return true;
+    }
+  });
+  
